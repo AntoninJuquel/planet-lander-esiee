fe6fd19d7d9a8bf83417093be7ce0aab70b6087c
Antonin <antonin.juquel@outlook.fr>
2022-05-19 00:58:21 +0200

    rename

--- Assets/Scripts/WorldGeneration/World.cs
+++ Assets/Scripts/WorldGeneration/World.cs
@@ -1,58 +1,30 @@
-﻿using System.Collections.Generic;
-using ReferenceSharing;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace WorldGeneration
 {
-    public class World : MonoBehaviour
+    [CreateAssetMenu(fileName = "New world preset", menuName = "World", order = 0)]
+    public class WorldPreset : ScriptableObject
     {
-        [SerializeField] private Reference<int> levelRef;
-        [SerializeField] private WorldPart worldPartPrefab;
-        [SerializeField] private WorldPreset[] worldPresets;
-        private readonly Dictionary<Vector2, WorldPart> _worldParts = new Dictionary<Vector2, WorldPart>();
-        private int PresetIndex => levelRef.Value % worldPresets.Length;
+        public float gravity;
+        public Vector2 step, height;
+        public WorldDeformation[] worldDeformations;
 
-        public void OnChunkCreated(Bounds e)
-        {
-            if (e.center.y != 0) return;
-            var start = e.center.x - e.size.x * .5f;
-            var end = e.center.x + e.size.y * .5f;
-            GenerateWorldPart(start, end, e.center.x);
-        }
+        public NoiseSettings[] noiseSettings;
+        public Color backgroundColor;
+    }
 
-        public void OnChunkEnabled(Bounds e)
-        {
-            if (e.center.y != 0) return;
-            _worldParts[e.center].gameObject.SetActive(true);
-        }
+    [System.Serializable]
+    public struct WorldDeformation
+    {
+        public float chance;
+        public Vector2 width, depth, resolution;
+    }
 
-        public void OnChunkDisabled(Bounds e)
-        {
-            if (e.center.y != 0) return;
-            _worldParts[e.center].gameObject.SetActive(false);
-        }
-
-        private void StartGameHandler()
-        {
-            foreach (var kvp in _worldParts)
-            {
-                kvp.Value.Generate(worldPresets[PresetIndex]);
-            }
-
-            Generate(PresetIndex);
-        }
-
-        private void Generate(int index)
-        {
-            Physics2D.gravity = Vector2.up * worldPresets[index].gravity;
-        }
-
-        private void GenerateWorldPart(float startX, float endX, float centerX)
-        {
-            Vector2 position = Vector3.right * centerX;
-            var worldPart = Instantiate(worldPartPrefab, Vector3.zero, Quaternion.identity, transform);
-            worldPart.Generate(worldPresets[PresetIndex], startX, endX);
-            _worldParts.Add(position, worldPart);
-        }
+    [System.Serializable]
+    public struct NoiseSettings
+    {
+        public string name;
+        public float strength;
+        public float roughness;
     }
 }
